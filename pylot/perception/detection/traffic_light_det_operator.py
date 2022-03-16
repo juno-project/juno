016552f73156b817d3817749f2e4ab4662c80a7d
haokunkun <kkhao21@163.com>
2022-03-16 15:04:58 +0800

    create zenoh-folw-driving

---
+++ pylot/perception/detection/traffic_light_det_operator.py
@@ -0,0 +1,122 @@
+"""Implements an operator that detects traffic lights."""
+import logging
+import os
+
+import numpy as np
+import pickle
+from pylot.perception.detection.traffic_light import TrafficLight, \
+    TrafficLightColor
+from pylot.perception.detection.utils import BoundingBox2D
+from pylot.perception.messages import TrafficLightsMessage
+
+import tensorflow as tf
+
+class TrafficLightDetState:
+    def __init__(self, cfg):
+        # Only sets memory growth for flagged GPU
+        # physical_devices = tf.config.experimental.list_physical_devices('GPU')
+        # tf.config.experimental.set_visible_devices(
+        #     [physical_devices[cfg['traffic_light_det_gpu_index']], 'GPU')
+        # tf.config.experimental.set_memory_growth(
+        #     physical_devices[cfg['traffic_light_det_gpu_index']], True)
+
+        # Load the model from the saved_model format file.
+        self.cfg = cfg
+        self._model = tf.saved_model.load(cfg['model_path'])
+
+            #self._flags.traffic_light_det_model_path)
+
+        self._labels = {
+            1: TrafficLightColor.GREEN,
+            2: TrafficLightColor.YELLOW,
+            3: TrafficLightColor.RED,
+            4: TrafficLightColor.OFF
+        }
+        # Unique bounding box id. Incremented for each bounding box.
+        self._unique_id = 0
+
+    def run_model(self, image_np):
+        # Expand dimensions since the model expects images to have
+        # shape: [1, None, None, 3]
+        image_np_expanded = np.expand_dims(image_np, axis=0)
+
+        infer = self._model.signatures['serving_default']
+        result = infer(tf.convert_to_tensor(value=image_np_expanded))
+
+        boxes = result['boxes']
+        scores = result['scores']
+        classes = result['classes']
+        num_detections = result['detections']
+
+        num_detections = int(num_detections[0])
+        res_labels = [
+            self._labels[int(label)] for label in classes[0][:num_detections]
+        ]
+        res_boxes = boxes[0][:num_detections]
+        res_scores = scores[0][:num_detections]
+        return res_boxes, res_scores, res_labels
+
+    def convert_to_detected_tl(self, boxes, scores, labels, height, width):
+        traffic_lights = []
+
+        for index in range(len(scores)):
+            if scores[
+                    index] > self.cfg['traffic_light_det_min_score_threshold']/10:
+                bbox = BoundingBox2D(
+                    int(boxes[index][1] * width),  # x_min
+                    int(boxes[index][3] * width),  # x_max
+                    int(boxes[index][0] * height),  # y_min
+                    int(boxes[index][2] * height)  # y_max
+                )
+                traffic_lights.append(
+                    TrafficLight(scores[index],
+                                 labels[index],
+                                 id=self._unique_id,
+                                 bounding_box=bbox))
+                self._unique_id += 1
+        return traffic_lights
+
+class TrafficLightDetOperator():
+    """Detects traffic lights using a TensorFlow model.
+
+    The operator receives frames on a camera stream, and runs a model for each
+    frame."""
+    def initialize(self, configuration):
+        return TrafficLightDetState(configuration)
+
+    def input_rule(self, _ctx, state, tokens):
+        # Using input rules
+        return True
+
+    def output_rule(self, _ctx, _state, outputs, _deadline_miss):
+        return outputs
+
+    def finalize(self, state):
+        return None
+
+    def run(self, _ctx, _state, inputs):
+        msg = inputs.get("FrameMsg").data
+        msg = pickle.loads(msg)
+
+        print('@{}: {} received message'.format(
+            msg.timestamp, 'TrafficLightDetOperator'))
+        assert msg.frame.encoding == 'BGR', 'Expects BGR frames'
+        boxes, scores, labels = _state.run_model(msg.frame.as_rgb_numpy_array())
+
+        traffic_lights = _state.convert_to_detected_tl(
+            boxes, scores, labels, msg.frame.camera_setup.height, msg.frame.camera_setup.width)
+
+        print('@{}: {} detected traffic lights {}'.format(
+            msg.timestamp, 'TrafficLightDetOperator', traffic_lights))
+
+        if _state.cfg['log_traffic_light_detector_output'] and traffic_lights:
+            msg.frame.annotate_with_bounding_boxes(msg.timestamp,
+                                                   traffic_lights)
+            os.makedirs(_state.cfg['out_path'], exist_ok=True)
+            msg.frame.save(msg.timestamp, _state.cfg['out_path'],
+                           'tl-detector-{}'.format('TrafficLightDetOperator'))
+
+        return {"TrafficLightsMsg": pickle.dumps(TrafficLightsMessage(msg.timestamp, traffic_lights))}
+
+def register():
+    return TrafficLightDetOperator
---
+++ sources/detection_source.py
@@ -0,0 +1,52 @@
+
+import sys
+sys.path.append('/home/kkhao/pycharmPro/zenoh-flow-auto-driving/')
+from zenoh_flow import Inputs, Outputs, Source
+import time
+import pickle
+import cv2
+import pylot.utils
+from pylot.drivers.sensor_setup import DepthCameraSetup, RGBCameraSetup, \
+    SegmentedCameraSetup
+from pylot.perception.camera_frame import CameraFrame
+from pylot.perception.messages import FrameMessage
+if not hasattr(sys, 'argv'):
+    sys.argv = ['']
+
+CENTER_CAMERA_LOCATION = pylot.utils.Location(1.3, 0.0, 1.8)
+transform = pylot.utils.Transform(CENTER_CAMERA_LOCATION,
+                                  pylot.utils.Rotation(pitch=-15))
+class MyState:
+    def __init__(self, cfg):
+        src = cv2.imread(cfg['image_path'])
+        # dst = cv2.cvtColor(src, cv2.COLOR_BGR2RGB)
+        self.value = src
+        self.timestamp = 0
+        self.center_camera_setup = RGBCameraSetup('center_camera',
+                                             self.value.shape[1],
+                                             self.value.shape[0], transform,
+                                             )
+class MySrc(Source):
+    def initialize(self, configuration):
+        return MyState(configuration)
+
+    def finalize(self, state):
+        return None
+
+    def run(self, _ctx, state):
+        print('state.timestamp', state.timestamp)
+        rgb_frame = CameraFrame(state.value, 'BGR', state.center_camera_setup)
+        msg = FrameMessage(state.timestamp, rgb_frame)
+        state.timestamp += 1
+        # print(pickle.dumps(msg))
+        time.sleep(1)
+        return pickle.dumps(msg)
+
+def register():
+    return MySrc
+
+if __name__=='__main__':
+    config = {'image_path': '/home/kkhao/pycharmPro/zenoh-flow-auto-driving/data/center-8711.png'}
+    camera = MySrc()
+    state = camera.initialize(config)
+    camera.run(None, state)
