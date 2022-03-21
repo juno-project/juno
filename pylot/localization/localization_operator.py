def505614e3d9a4ee5fcdf8365bb99d902a8d220
qhh <15029670296@163.com>
2022-03-21 16:12:22 +0800

    transplant  LocalizationOperator

--- pylot/localization/localization_operator.py
+++ pylot/localization/localization_operator.py
@@ -1,63 +1,26 @@
 """This module implements EKF localization using GNSS and IMU."""
-
+import pickle
+import sys
+import time
 from collections import deque
-from functools import partial
-
-import erdos
-from erdos import Message, ReadStream, Timestamp, WriteStream
-
 import numpy as np
-
+sys.path.append('/home/erdos/zenoh-flow-auto-driving/')
+from pylot.localization.messages import PoseMessage
 from pylot.utils import Location, Pose, Quaternion, Rotation, Transform, \
-        Vector3D
+    Vector3D
 
 
-class LocalizationOperator(erdos.Operator):
-    """Localizes vehicle using GPS and IMU.
-
-    The operator implements localization using a Kalman filter.
-
-    Args:
-        imu_stream (:py:class:`erdos.ReadStream`): Stream on which IMU
-            sensors readings are received.
-        gnss_stream (:py:class:`erdos.ReadStream`): Stream on which
-            GNSS readings are received.
-        ground_pose_stream (:py:class:`erdos.ReadStream`): Stream on which
-            the operator receives noisy poses. It refines these poses using
-            GPS and IMU.
-        pose_stream (:py:class:`erdos.ReadStream`): Stream on which the
-            operator outputs pose messages.
-        flags (absl.flags): Object to be used to access absl flags.
-    """
-    def __init__(self, imu_stream: ReadStream, gnss_stream: ReadStream,
-                 ground_pose_stream: ReadStream, pose_stream: WriteStream,
-                 flags):
+class LocalizationState:
+    def  __init__(self, cfg):
         # Register callbacks on read streams.
         self._imu_updates = deque()
-        imu_stream.add_callback(
-            partial(self.buffer_msg, msg_type="IMU", queue=self._imu_updates))
 
         self._gnss_updates = deque()
-        gnss_stream.add_callback(
-            partial(self.buffer_msg, msg_type="GNSS",
-                    queue=self._gnss_updates))
 
         self._ground_pose_updates = deque()
-        ground_pose_stream.add_callback(
-            partial(self.buffer_msg,
-                    msg_type="pose",
-                    queue=self._ground_pose_updates))
-        erdos.add_watermark_callback(
-            [imu_stream, gnss_stream, ground_pose_stream], [],
-            self.on_watermark)
-
-        # Save the output stream.
-        self._pose_stream = pose_stream
 
         # Initialize a logger.
-        self._flags = flags
-        self._logger = erdos.utils.setup_logging(self.config.name,
-                                                 self.config.log_file_name)
+        self.cfg = cfg
 
         # Gravity vector.
         self._g = np.array([0, 0, -9.81])
@@ -87,41 +50,7 @@ class LocalizationOperator(erdos.Operator):
 
         self._last_covariance = np.zeros((9, 9))
 
-    @staticmethod
-    def connect(imu_stream: ReadStream, gnss_stream: ReadStream,
-                ground_pose_stream: ReadStream):
-        pose_stream = erdos.WriteStream()
-        return [pose_stream]
-
-    def destroy(self):
-        self._logger.warn('destroying {}'.format(self.config.name))
-
-    def buffer_msg(self, msg: Message, msg_type: str, queue: deque):
-        """Callback which buffers the received message."""
-        self._logger.debug("@{}: received {} message.".format(
-            msg.timestamp, msg_type))
-        queue.append(msg)
-
-    def get_message(self, queue: deque, timestamp: Timestamp, name: str):
-        msg = None
-        if queue:
-            while len(queue) > 0:
-                retrieved_msg = queue.popleft()
-                if retrieved_msg.timestamp == timestamp:
-                    msg = retrieved_msg
-                    break
-            if not msg:
-                raise ValueError(
-                    "The message for {} with timestamp {} was not found".
-                    format(name, timestamp))
-        return msg
-
-    def __skew_symmetric(self, v):
-        """Skew symmetric form of a 3x1 vector."""
-        return np.array([[0, -v[2], v[1]], [v[2], 0, -v[0]], [-v[1], v[0], 0]],
-                        dtype=np.float64)
-
-    def __update_using_gnss(self, location_estimate, velocity_estimate,
+    def update_using_gnss(self, location_estimate, velocity_estimate,
                             rotation_estimate, gnss_reading, delta_t):
         # Construct H_k = [I, 0, 0] (shape=(3, 9))
         H_k = np.zeros((3, 9))
@@ -159,61 +88,112 @@ class LocalizationOperator(erdos.Operator):
             corrected_rotation_estimate,
         )
 
-    @erdos.profile_method()
-    def on_watermark(self, timestamp: Timestamp):
-        self._logger.debug("@{}: received watermark.".format(timestamp))
-        if timestamp.is_top:
-            self._pose_stream.send(erdos.WatermarkMessage(timestamp))
-            return
+    def buffer_msg(self, msg, msg_type: str, queue: deque):
+        """Callback which buffers the received message."""
+        print("@{}: received {} message.".format(
+            msg.timestamp, msg_type))
+        queue.append(msg)
 
-        # Retrieve the messages for this timestamp.
-        pose_msg = self.get_message(self._ground_pose_updates, timestamp,
-                                    "pose")
-        gnss_msg = self.get_message(self._gnss_updates, timestamp, "GNSS")
-        imu_msg = self.get_message(self._imu_updates, timestamp, "IMU")
+    def get_message(self, queue: deque, timestamp, name: str):
+        msg = None
+        if queue:
+            while len(queue) > 0:
+                retrieved_msg = queue.popleft()
+                if retrieved_msg.timestamp == timestamp:
+                    msg = retrieved_msg
+                    break
+            if not msg:
+                raise ValueError(
+                    "The message for {} with timestamp {} was not found".
+                    format(name, timestamp))
+        return msg
 
-        if self._last_pose_estimate is None or \
-           (abs(imu_msg.acceleration.y) > 100 and not self._is_started):
-            self._logger.debug(
-                "@{}: The initial pose estimate is not initialized.".format(
-                    timestamp))
+    def __skew_symmetric(self, v):
+        """Skew symmetric form of a 3x1 vector."""
+        return np.array([[0, -v[2], v[1]], [v[2], 0, -v[0]], [-v[1], v[0], 0]],
+                        dtype=np.float64)
+
+class LocalizationOperator():
+    def initialize(self, configuration):
+        return LocalizationState(configuration)
+
+    def input_rule(self, _ctx, state, tokens):
+        # Using input rules
+        token = tokens.get('localizationMsg').get_data()
+        msg = pickle.loads(bytes(token))
+
+        pose_msg = msg['pose']
+        gnss_msg = msg['gnss']
+        imu_msg = msg['imu']
+
+        state.buffer_msg(pose_msg, msg_type="pose", queue=state._ground_pose_updates)
+        state.buffer_msg(gnss_msg, msg_type="GNSS", queue=state._gnss_updates)
+        state.buffer_msg(imu_msg, msg_type="IMU", queue=state._imu_updates)
+
+        return True
+
+    def output_rule(self, _ctx, _state, outputs, _deadline_miss):
+        return outputs
+
+    def finalize(self, state):
+        return None
+
+    def run(self, _ctx, _state, inputs):
+        msg = inputs.get("localizationMsg").data
+        # print("@: inputs:  {}".format(msg))
+        msg = pickle.loads(msg)
+
+        # # Retrieve the messages for this timestamp
+        # pose_msg = msg['pose']
+        # gnss_msg = msg['gnss']
+        # imu_msg = msg['imu']
+        timestamp = msg['timestamp']
+
+        print("@{}: received watermark.".format(timestamp))
+        # if timestamp.is_top:
+        #     self._pose_stream.send(erdos.WatermarkMessage(timestamp))
+        #     return
+        pose_msg = _state.get_message(_state._ground_pose_updates, msg['pose'].timestamp,"pose")
+        gnss_msg = _state.get_message(_state._gnss_updates, msg['gnss'].timestamp, "GNSS")
+        imu_msg = _state.get_message(_state._imu_updates, msg['imu'].timestamp, "IMU")
+
+        if _state._last_pose_estimate is None or \
+                (abs(imu_msg.acceleration.y) > 100 and not _state._is_started):
+            print("@{}: The initial pose estimate is not initialized.".format(timestamp))
             # If this is the first update or values have not stabilized,
             # save the estimates.
             if pose_msg:
-                self._last_pose_estimate = pose_msg.data
-                self._last_timestamp = timestamp.coordinates[0]
-                self._pose_stream.send(pose_msg)
-                self._pose_stream.send(erdos.WatermarkMessage(timestamp))
+                _state._last_pose_estimate = pose_msg.data
+                _state._last_timestamp = time.time()
             else:
-                raise NotImplementedError(
-                    "Need pose message to initialize the estimates.")
+                raise NotImplementedError("Need pose message to initialize the estimates.")
         else:
-            self._is_started = True
+            _state._is_started = True
 
             # Initialize the delta_t
-            current_ts = timestamp.coordinates[0]
-            delta_t = (current_ts - self._last_timestamp) / 1000.0
+            current_ts = time.time()
+            delta_t = (current_ts - _state._last_timestamp) / 1000.0
 
             # Estimate the rotation.
             last_rotation_estimate = Quaternion.from_rotation(
-                self._last_pose_estimate.transform.rotation)
+                _state._last_pose_estimate.transform.rotation)
             rotation_estimate = (
-                last_rotation_estimate *
-                Quaternion.from_angular_velocity(imu_msg.gyro, delta_t))
+                    last_rotation_estimate *
+                    Quaternion.from_angular_velocity(imu_msg.gyro, delta_t))
 
             # Transform the IMU accelerometer data from the body frame to the
             # world frame, and retrieve location and velocity estimates.
             accelerometer_data = last_rotation_estimate.matrix.dot(
-                imu_msg.acceleration.as_numpy_array()) + self._g
+                imu_msg.acceleration.as_numpy_array()) + _state._g
             last_location_estimate = \
-                self._last_pose_estimate.transform.location.as_numpy_array()
+                _state._last_pose_estimate.transform.location.as_numpy_array()
             last_velocity_estimate = \
-                self._last_pose_estimate.velocity_vector.as_numpy_array()
+                _state._last_pose_estimate.velocity_vector.as_numpy_array()
 
             # Estimate the location.
             location_estimate = last_location_estimate + (
-                delta_t * last_velocity_estimate) + ((
-                    (delta_t**2) / 2.0) * accelerometer_data)
+                    delta_t * last_velocity_estimate) + ((
+                                                                 (delta_t ** 2) / 2.0) * accelerometer_data)
 
             # Estimate the velocity.
             velocity_estimate = last_velocity_estimate + (delta_t *
@@ -223,9 +203,9 @@ class LocalizationOperator(erdos.Operator):
             # the estimates.
 
             # Linearize the motion model and compute Jacobians.
-            self.__F[0:3, 3:6] = np.identity(3) * delta_t
-            self.__F[3:6, 6:9] = last_rotation_estimate.matrix.dot(
-                -self.__skew_symmetric(accelerometer_data.reshape(
+            _state._LocalizationState__F[0:3, 3:6] = np.identity(3) * delta_t
+            _state._LocalizationState__F[3:6, 6:9] = last_rotation_estimate.matrix.dot(
+                -_state._LocalizationState__skew_symmetric(accelerometer_data.reshape(
                     (3, 1)))) * delta_t
 
             # Fix estimates using GNSS
@@ -236,7 +216,7 @@ class LocalizationOperator(erdos.Operator):
                 location_estimate,
                 velocity_estimate,
                 rotation_estimate,
-            ) = self.__update_using_gnss(location_estimate, velocity_estimate,
+            ) = _state.update_using_gnss(location_estimate, velocity_estimate,
                                          rotation_estimate, gnss_reading,
                                          delta_t)
 
@@ -248,11 +228,15 @@ class LocalizationOperator(erdos.Operator):
                 velocity_vector=Vector3D(*velocity_estimate),
                 localization_time=current_ts,
             )
-            self._logger.debug("@{}: Predicted pose: {}".format(
+            print("@{}: Predicted pose: {}".format(
                 timestamp, current_pose))
-            self._pose_stream.send(erdos.Message(timestamp, current_pose))
-            self._pose_stream.send(erdos.WatermarkMessage(timestamp))
 
             # Set the estimates for the next iteration.
-            self._last_timestamp = current_ts
-            self._last_pose_estimate = current_pose
+            _state._last_timestamp = current_ts
+            _state._last_pose_estimate = current_pose
+
+        return {'PoseMessage': pickle.dumps(PoseMessage(_state._last_timestamp, _state._last_pose_estimate))}
+
+
+def register():
+    return LocalizationOperator
---
+++ sinks/localization_sink.py
@@ -0,0 +1,20 @@
+import os
+import pickle
+
+from zenoh_flow import Sink
+class MySink(Sink):
+    def initialize(self, configuration):
+        return None
+
+    def finalize(self, state):
+        return None
+
+    def run(self, _ctx, _state, input):
+        data = pickle.loads(input.data)
+        print("sink input_data : {}".format(data))
+        out_poss_path = os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + os.path.sep + ".")
+        out_poss_path = os.path.join(out_poss_path, "test_data/LocalizationOperator/output/pose/pose-5643.pkl")
+        out_data = pickle.load(open(out_poss_path, "rb"))
+        print("sink out_data : {}".format(out_data))
+def register():
+    return MySink
