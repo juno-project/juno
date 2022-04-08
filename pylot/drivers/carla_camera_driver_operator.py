c3b4efb146c748e673c220f3bb1a276c861a8c82
qhh <15029670296@163.com>
2022-04-08 16:35:56 +0800

    add carla_carmer_driver

--- pylot/drivers/carla_camera_driver_operator.py
+++ pylot/drivers/carla_camera_driver_operator.py
@@ -4,22 +4,35 @@ using the simulator.
 The operator attaches a camera to the ego vehicle, receives camera frames from
 the simulator, and sends them on its output stream.
 """
-
+import logging
 import pickle
 import threading
-
-import erdos
-
+import time
+import pylot.utils
+from pylot.drivers.sensor_setup import RGBCameraSetup
 from pylot.perception.camera_frame import CameraFrame
 from pylot.perception.depth_frame import DepthFrame
 from pylot.perception.messages import DepthFrameMessage, FrameMessage, \
-    SegmentedFrameMessage
+    SegmentedFrameMessage, Message
 from pylot.perception.segmentation.segmented_frame import SegmentedFrame
 from pylot.simulation.utils import get_vehicle_handle, get_world, \
     set_simulation_mode
 
 
-class CarlaCameraDriverOperator(erdos.Operator):
+
+
+def get_logger(log_file_name):
+    logger = logging.getLogger(__name__)
+    logger.setLevel(level=logging.INFO)
+    handler = logging.FileHandler(log_file_name)
+    handler.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    return logger
+
+
+class CarlaCameraDriverState:
     """Publishes images onto the desired stream from a camera.
 
     This operator attaches a vehicle at the required position with respect to
@@ -38,19 +51,35 @@ class CarlaCameraDriverOperator(erdos.Operator):
             Setup of the camera.
         flags (absl.flags): Object to be used to access absl flags.
     """
-    def __init__(self, ground_vehicle_id_stream: erdos.ReadStream,
-                 release_sensor_stream: erdos.ReadStream,
-                 camera_stream: erdos.WriteStream,
-                 notify_reading_stream: erdos.WriteStream, camera_setup,
-                 flags):
-        erdos.add_watermark_callback([release_sensor_stream], [],
-                                     self.release_data)
-        self._vehicle_id_stream = ground_vehicle_id_stream
-        self._camera_stream = camera_stream
-        self._notify_reading_stream = notify_reading_stream
-        self._flags = flags
-        self._logger = erdos.utils.setup_logging(self.config.name,
-                                                 self.config.log_file_name)
+
+    def __init__(self, cfg):
+        # erdos.add_watermark_callback([release_sensor_stream], [],
+        #                              self.release_data)
+        # self._vehicle_id_stream = ground_vehicle_id_stream
+        # self._camera_stream = camera_stream
+        # self._notify_reading_stream = notify_reading_stream
+
+        self.cfg = cfg
+        # self._logger = erdos.utils.setup_logging(self.config.name,
+        #                                          self.config.log_file_name)
+
+        self._logger = get_logger(cfg["log_file_name"])
+
+        # center_camera_setup = RGBCameraSetup('center_camera',
+        #                                      FLAGS.camera_image_width,
+        #                                      FLAGS.camera_image_height, transform,
+        #                                      FLAGS.camera_fov)
+
+        CENTER_CAMERA_LOCATION = pylot.utils.Location(1.3, 0.0, 1.8)
+        transform = pylot.utils.Transform(CENTER_CAMERA_LOCATION,
+                                          pylot.utils.Rotation(pitch=-15))
+        camera_setup = RGBCameraSetup(cfg["camera_name"],
+                                      cfg["camera_image_width"],
+                                      cfg["camera_image_height"], transform,
+                                      cfg["camera_fov"])
+
+        self.notify_reading_msg = None
+        self.camera_msg = None
         self._camera_setup = camera_setup
         # The hero vehicle actor object we obtain from the simulator.
         self._vehicle = None
@@ -65,114 +94,169 @@ class CarlaCameraDriverOperator(erdos.Operator):
         # receives it.
         self._release_data = False
 
-    @staticmethod
-    def connect(ground_vehicle_id_stream: erdos.ReadStream,
-                release_sensor_stream: erdos.ReadStream):
-        camera_stream = erdos.WriteStream()
-        notify_reading_stream = erdos.WriteStream()
-        return [camera_stream, notify_reading_stream]
+    def process_images(self, simulator_image):
+        """Invoked when an image is received from the simulator."""
+        game_time = int(simulator_image.timestamp * 1000)
+        # timestamp = erdos.Timestamp(coordinates=[game_time])
+        # watermark_msg = erdos.WatermarkMessage(timestamp)
+        timestamp = time.time()
+        watermark_msg = timestamp
+        # with erdos.profile(self.config.name + '.process_images',
+        #                    self,
+        #                    event_data={'timestamp': str(timestamp)}):
+        # Ensure that the code executes serially
+        with self._lock:
+            msg = None
+            if self._camera_setup.camera_type == 'sensor.camera.rgb':
+                msg = FrameMessage(
+                    timestamp,
+                    CameraFrame.from_simulator_frame(
+                        simulator_image, self._camera_setup))
+            elif self._camera_setup.camera_type == 'sensor.camera.depth':
+                # Include the transform relative to the vehicle.
+                # simulator_image.transform returns the world transform,
+                # but we do not use it directly.
+                msg = DepthFrameMessage(
+                    timestamp,
+                    DepthFrame.from_simulator_frame(
+                        simulator_image,
+                        self._camera_setup,
+                        save_original_frame=self.cfg["visualize_depth_camera"] == "True"))
+            elif (self._camera_setup.camera_type ==
+                  'sensor.camera.semantic_segmentation'):
+                msg = SegmentedFrameMessage(
+                    timestamp,
+                    SegmentedFrame.from_simulator_image(
+                        simulator_image, self._camera_setup))
 
-    @erdos.profile_method()
+            if self._release_data:
+                # self.cfg._camera_stream.send(msg)
+                # self.cfg._camera_stream.send(watermark_msg)
+                self.camera_msg = msg
+            else:
+                # Pickle the data, and release it upon release msg receipt.
+                pickled_msg = pickle.dumps(
+                    msg, protocol=pickle.HIGHEST_PROTOCOL)
+                with self._pickle_lock:
+                    self._pickled_messages[msg.timestamp] = pickled_msg
+                # self._notify_reading_stream.send(watermark_msg)
+                self.notify_reading_msg = watermark_msg
+
     def release_data(self, timestamp):
-        if timestamp.is_top:
-            # The operator can always send data ASAP.
-            self._release_data = True
-        else:
-            self._logger.debug("@{}: {} releasing sensor data".format(
-                timestamp, self.config.name))
-            watermark_msg = erdos.WatermarkMessage(timestamp)
-            self._camera_stream.send_pickled(timestamp,
-                                             self._pickled_messages[timestamp])
-            # Note: The operator is set not to automatically propagate
-            # watermark messages received on input streams. Thus, we can
-            # issue watermarks only after the simulator callback is invoked.
-            self._camera_stream.send(watermark_msg)
-            with self._pickle_lock:
-                del self._pickled_messages[timestamp]
+        self._release_data = True
+        # if timestamp.is_top:
+        #     # The operator can always send data ASAP.
+        #     self._release_data = True
+        # else:
+        #     self._logger.debug("@{}: {} releasing sensor data".format(
+        #         timestamp, self.config.name))
+        #     watermark_msg = erdos.WatermarkMessage(timestamp)
+        #     self._camera_stream.send_pickled(timestamp,
+        #                                      self._pickled_messages[timestamp])
+        #     # Note: The operator is set not to automatically propagate
+        #     # watermark messages received on input streams. Thus, we can
+        #     # issue watermarks only after the simulator callback is invoked.
+        #     self._camera_stream.send(watermark_msg)
+        #     with self._pickle_lock:
+        #         del self._pickled_messages[timestamp]
 
-    def run(self):
+
+class CarlaCameraDriverOperator():
+
+    def initialize(self, configuration):
+        return CarlaCameraDriverState(configuration)
+
+    def input_rule(self, _ctx, state, tokens):
+        # Using input rules
+        token = tokens.get('carlaOperatorMsg').get_data()
+        msg = pickle.loads(bytes(token))
+
+        state.vehicle_id_msg = msg['vehicle_id_msg']
+        timestamp = msg['timestamp']
+        state.release_data(timestamp)
+
+        # state.vehicle_id_msg = pickle.load(
+        #     open("/home/erdos/workspace/zenoh-flow-auto-driving/test_data/CarlaCameraDriverOperator/input/vehicle_id_msg.pkl",
+        #          "rb"))
+        # print(state.vehicle_id_msg)
+        # state.release_data(time.time())
+
+
+        return True
+
+    def output_rule(self, _ctx, _state, outputs, _deadline_miss):
+        return outputs
+
+    def finalize(self, state):
+        return None
+
+    def run(self, _ctx, _state, inputs):
         # Read the vehicle id from the vehicle id stream
-        vehicle_id_msg = self._vehicle_id_stream.read()
-        vehicle_id = vehicle_id_msg.data
-        self._logger.debug(
+        vehicle_id = _state.vehicle_id_msg.data
+        print(
             "The CameraDriverOperator received the vehicle id: {}".format(
                 vehicle_id))
 
         # Connect to the world. We connect here instead of in the constructor
         # to ensure we're connected to the latest world.
-        _, world = get_world(self._flags.simulator_host,
-                             self._flags.simulator_port,
-                             self._flags.simulator_timeout)
-        set_simulation_mode(world, self._flags)
+        _, world = get_world(_state.cfg["simulator_host"],
+                             _state.cfg["simulator_port"],
+                             _state.cfg["simulator_timeout"])
+
+        set_simulation_mode(world, _state.cfg)
 
         self._vehicle = get_vehicle_handle(world, vehicle_id)
+        print("self._vehicle :{}".format(self._vehicle))
 
         # Install the camera.
         camera_blueprint = world.get_blueprint_library().find(
-            self._camera_setup.camera_type)
+            _state._camera_setup.camera_type)
         camera_blueprint.set_attribute('image_size_x',
-                                       str(self._camera_setup.width))
+                                       str(_state._camera_setup.width))
         camera_blueprint.set_attribute('image_size_y',
-                                       str(self._camera_setup.height))
-        camera_blueprint.set_attribute('fov', str(self._camera_setup.fov))
-        if self._flags.simulator_camera_frequency == -1:
+                                       str(_state._camera_setup.height))
+        camera_blueprint.set_attribute('fov', str(_state._camera_setup.fov))
+        if _state.cfg["simulator_camera_frequency"] == -1:
             camera_blueprint.set_attribute('sensor_tick', '0.0')
         else:
             camera_blueprint.set_attribute(
                 'sensor_tick',
-                str(1.0 / self._flags.simulator_camera_frequency))
+                str(1.0 / _state.cfg["simulator_camera_frequency"]))
 
-        transform = self._camera_setup.get_transform().as_simulator_transform()
+        transform = _state._camera_setup.get_transform().as_simulator_transform()
 
-        self._logger.debug("Spawning a camera: {}".format(self._camera_setup))
-        self._camera = world.spawn_actor(camera_blueprint,
-                                         transform,
-                                         attach_to=self._vehicle)
+        _state._logger.debug("Spawning a camera: {}".format(_state._camera_setup))
+        _state._camera = world.spawn_actor(camera_blueprint,
+                                           transform,
+                                           attach_to=self._vehicle)
 
         # Register the callback on the camera.
-        self._camera.listen(self.process_images)
+        _state._camera.listen(_state.process_images)
+        return {'carlaCameraDriverMsg': pickle.dumps(_state.camera_msg)}
 
-    def process_images(self, simulator_image):
-        """Invoked when an image is received from the simulator."""
-        game_time = int(simulator_image.timestamp * 1000)
-        timestamp = erdos.Timestamp(coordinates=[game_time])
-        watermark_msg = erdos.WatermarkMessage(timestamp)
-        with erdos.profile(self.config.name + '.process_images',
-                           self,
-                           event_data={'timestamp': str(timestamp)}):
-            # Ensure that the code executes serially
-            with self._lock:
-                msg = None
-                if self._camera_setup.camera_type == 'sensor.camera.rgb':
-                    msg = FrameMessage(
-                        timestamp,
-                        CameraFrame.from_simulator_frame(
-                            simulator_image, self._camera_setup))
-                elif self._camera_setup.camera_type == 'sensor.camera.depth':
-                    # Include the transform relative to the vehicle.
-                    # simulator_image.transform returns the world transform,
-                    # but we do not use it directly.
-                    msg = DepthFrameMessage(
-                        timestamp,
-                        DepthFrame.from_simulator_frame(
-                            simulator_image,
-                            self._camera_setup,
-                            save_original_frame=self._flags.
-                            visualize_depth_camera))
-                elif (self._camera_setup.camera_type ==
-                      'sensor.camera.semantic_segmentation'):
-                    msg = SegmentedFrameMessage(
-                        timestamp,
-                        SegmentedFrame.from_simulator_image(
-                            simulator_image, self._camera_setup))
 
-                if self._release_data:
-                    self._camera_stream.send(msg)
-                    self._camera_stream.send(watermark_msg)
-                else:
-                    # Pickle the data, and release it upon release msg receipt.
-                    pickled_msg = pickle.dumps(
-                        msg, protocol=pickle.HIGHEST_PROTOCOL)
-                    with self._pickle_lock:
-                        self._pickled_messages[msg.timestamp] = pickled_msg
-                    self._notify_reading_stream.send(watermark_msg)
+def register():
+    return CarlaCameraDriverOperator
+
+
+if __name__ == '__main__':
+
+    config = {
+        "log_file_name": 'pylot.log',
+        "camera_name": 'center_camera_operator',
+        "camera_image_width": 1920,
+        "camera_image_height": 1080,
+        "camera_fov": 90.0,
+        "visualize_depth_camera": False,
+        "name": "center_camera_operator",
+        "simulator_host": 'localhost',
+        "simulator_port": 2000,
+        "simulator_timeout": 10,
+        "simulator_camera_frequency": -1,
+        "simulator_mode": 'synchronous',
+        "simulator_fps": 20
+    }
+    operator = CarlaCameraDriverOperator()
+    state = operator.initialize(config)
+    operator.input_rule(None, state, None)
+    operator.run(None, state, None)
