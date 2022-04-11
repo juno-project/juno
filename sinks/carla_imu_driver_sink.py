36b2a5b92afae1b0b1f00a4a28502880a0cde886
qhh <15029670296@163.com>
2022-04-11 14:32:00 +0800

    add carla_imu_driver_operator

---
+++ sinks/carla_imu_driver_sink.py
@@ -0,0 +1,21 @@
+import os
+import pickle
+
+from zenoh_flow import Sink
+class MySink(Sink):
+    def initialize(self, configuration):
+        print("sink init.............")
+        return None
+
+    def finalize(self, state):
+        return None
+
+    def run(self, _ctx, _state, input):
+        data = pickle.loads(input.data)
+        print("sink imu_msg----------------------------- : {}".format(data))
+def register():
+    return MySink
+
+
+
+
