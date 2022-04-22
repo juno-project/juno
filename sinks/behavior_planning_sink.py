2172ea70b7ce62aafd08efde0edd8cc91d675591
qhh <15029670296@163.com>
2022-04-22 09:38:46 +0800

    add links-1

--- sinks/behavior_planning_sink.py
+++ sinks/behavior_planning_sink.py
@@ -12,11 +12,11 @@ class MySink(Sink):
 
     def run(self, _ctx, _state, input):
         data = pickle.loads(input.data)
-        print("sink input_data : {}".format(data.waypoints.waypoints))
-        out_poss_path = os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + os.path.sep + ".")
-        out_poss_path = os.path.join(out_poss_path, "test_data/BehaviorPlanningOperator/output/trajectory_msg-5855.pkl")
-        out_data = pickle.load(open(out_poss_path, "rb"))
-        print("sink out_data : {}".format(out_data.waypoints.waypoints))
+        print("behavior sink : {}".format(data))
+        # out_poss_path = os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + os.path.sep + ".")
+        # out_poss_path = os.path.join(out_poss_path, "test_data/BehaviorPlanningOperator/output/trajectory_msg-5855.pkl")
+        # out_data = pickle.load(open(out_poss_path, "rb"))
+        # print("sink out_data : {}".format(out_data.waypoints.waypoints))
 def register():
     return MySink
 
