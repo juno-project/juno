eb5624ec1bd6a60440688e4a55b9d056df63f9e3
xiaofei <xiaofei@guardstrike.com>
2022-04-29 06:29:37 +0000

    update links-6

--- pylot/perception/tracking/object_tracker_operator.py
+++ pylot/perception/tracking/object_tracker_operator.py
@@ -1,7 +1,5 @@
-import time
-
 import pickle
-from matplotlib.pyplot import cla
+import time
 
 from pylot.perception.messages import ObstaclesMessage
 
@@ -12,11 +10,23 @@ class ObjectTrackerState:
         self._last_tracker_run_completion_time = 0
         self.detection_update_count = -1
         self.track_every_nth_detection = 1
-        self.tracker_type = cfg['tracker_type']
-        self.min_matching_iou = float(cfg['min_matching_iou'])
-        self.obstacle_track_max_age = cfg['obstacle_track_max_age']
-        self.da_siam_rpn_model_path = cfg['da_siam_rpn_model_path']
-        self.deep_sort_tracker_weights_path = cfg['deep_sort_tracker_weights_path']
+        min_matching_iou = float(cfg['min_matching_iou'])
+        obstacle_track_max_age = cfg['obstacle_track_max_age']
+        da_siam_rpn_model_path = cfg['da_siam_rpn_model_path']
+        deep_sort_tracker_weights_path = cfg['deep_sort_tracker_weights_path']
+        tracker_type = cfg['tracker_type']
+        if tracker_type == 'da_siam_rpn':
+            from pylot.perception.tracking.da_siam_rpn_tracker import \
+                MultiObjectDaSiamRPNTracker
+            self.tracker = MultiObjectDaSiamRPNTracker(da_siam_rpn_model_path, min_matching_iou, obstacle_track_max_age)
+        elif tracker_type == 'deep_sort':
+            from pylot.perception.tracking.deep_sort_tracker import \
+                MultiObjectDeepSORTTracker
+            self.tracker = MultiObjectDeepSORTTracker(deep_sort_tracker_weights_path, obstacle_track_max_age, min_matching_iou)
+        elif tracker_type == 'sort':
+            from pylot.perception.tracking.sort_tracker import \
+                MultiObjectSORTTracker
+            self.tracker = MultiObjectSORTTracker(obstacle_track_max_age, min_matching_iou)
 
 
 class ObjectTrackerOperator:
@@ -27,27 +37,22 @@ class ObjectTrackerOperator:
         return None
 
     def input_rule(self, _ctx, state, tokens):
-        print("object tracker operator received msgs.")
-        obstacle_token = tokens.get('ObstaclesWithLocationMsg')
+        obstacle_token = tokens.get('ObstaclesStream')
         camera_token = tokens.get('carlaCameraDriverMsg')
         if camera_token.is_pending():
-            print("camera pending")
             camera_token.set_action_keep()
             return False
         if obstacle_token.is_pending():
-            print("obstacle pending")
-            obstacle_token.set_action_drop()
+            obstacle_token.set_action_keep()
             return False
 
         if not obstacle_token.is_pending() and not camera_token.is_pending():
-            print("neither pending")
             obstacles_msg = pickle.loads(bytes(obstacle_token.get_data()))
             camera_msg = pickle.loads(bytes(camera_token.get_data()))
 
             camera_stream = camera_msg['camera_stream']
 
             if obstacles_msg is None or camera_stream is None:
-                print("either none")
                 obstacle_token.set_action_drop()
                 camera_token.set_action_drop()
                 return False
@@ -59,45 +64,44 @@ class ObjectTrackerOperator:
         return outputs
 
     def run(self, _ctx, _state, inputs):
-        print("erererere")
-        # frame_msg = _state.camera_stream
-        # camera_frame = frame_msg.frame
-        # obstacles_msg = _state.obstacles_msg
-        # timestamp = frame_msg.timestamp
-        # print('@{}: received watermark'.format(timestamp))
-        #
-        # tracked_obstacles = []
-        # detector_runtime = 0
-        # reinit_runtime = 0
-        # # Check if the most recent obstacle message has this timestamp.
-        # # If it doesn't, then the detector might have skipped sending
-        # # an obstacle message.
-        # if (len(obstacles_msg.obstacles) > 0
-        #         and obstacles_msg.timestamp == timestamp):
-        #     _state.detection_update_count += 1
-        #     if _state.detection_update_count % _state.track_every_nth_detection == 0:
-        #         # Reinitialize the tracker with new detections.
-        #         print(
-        #             'Restarting trackers at frame {}'.format(timestamp))
-        #         detected_obstacles = []
-        #         for obstacle in obstacles_msg.obstacles:
-        #             if obstacle.is_vehicle() or obstacle.is_person():
-        #                 detected_obstacles.append(obstacle)
-        #         reinit_runtime, _ = self._reinit_tracker(
-        #             camera_frame, detected_obstacles)
-        #         detector_runtime = obstacles_msg.runtime
-        # tracker_runtime, (ok, tracked_obstacles) = \
-        #     self._run_tracker(camera_frame)
-        # assert ok, 'Tracker failed at timestamp {}'.format(timestamp)
-        # tracker_runtime = tracker_runtime + reinit_runtime
-        # tracker_delay = self.__compute_tracker_delay(timestamp.coordinates[0],
-        #                                              detector_runtime,
-        #                                              tracker_runtime,
-        #                                              _state)
-        # return {'ObstaclesMessage': pickle.dumps(ObstaclesMessage(timestamp, tracked_obstacles, None, tracker_delay))}
-        return {'ObstaclesMessage': pickle.dumps(None)}
+        frame_msg = _state.camera_stream
+        camera_frame = frame_msg.frame
+        obstacles_msg = _state.obstacles_msg
+        camera_setup = obstacles_msg.camera_setup
+        timestamp = frame_msg.timestamp
+        detector_runtime = 0
+        reinit_runtime = 0
+        print("object tracker input obstacles: {}".format(obstacles_msg.obstacles))
+        # Check if the most recent obstacle message has this timestamp.
+        # If it doesn't, then the detector might have skipped sending
+        # an obstacle message.
+        if (len(obstacles_msg.obstacles) > 0
+                and obstacles_msg.timestamp == timestamp):
+            _state.detection_update_count += 1
+            if _state.detection_update_count % _state.track_every_nth_detection == 0:
+                # Reinitialize the tracker with new detections.
+                print(
+                    'Restarting trackers at frame {}'.format(timestamp))
+                detected_obstacles = []
+                for obstacle in obstacles_msg.obstacles:
+                    if obstacle.is_vehicle() or obstacle.is_person():
+                        detected_obstacles.append(obstacle)
+                reinit_runtime, _ = self._reinit_tracker(
+                   _state, camera_frame, detected_obstacles)
+                detector_runtime = obstacles_msg.runtime
+        tracker_runtime, (ok, tracked_obstacles) = \
+            self._run_tracker(_state, camera_frame)
+        assert ok, 'Tracker failed at timestamp {}'.format(timestamp)
+        tracker_runtime = tracker_runtime + reinit_runtime
+        tracker_delay = self.__compute_tracker_delay(timestamp,
+                                                     detector_runtime,
+                                                     tracker_runtime,
+                                                     _state)
+        print("object tracker output obstacles: {}".format(tracked_obstacles))
+        return {'ObstaclesHistoryTrackingStream': pickle.dumps(ObstaclesMessage(timestamp, tracked_obstacles, camera_setup, tracker_delay))}
 
-    def __compute_tracker_delay(self, world_time, detector_runtime,
+    @staticmethod
+    def __compute_tracker_delay(world_time, detector_runtime,
                                 tracker_runtime, _state):
         # If the tracker runtime does not fit within the frame gap, then
         # the tracker will fall behind. We need a scheduler to better
@@ -118,14 +122,16 @@ class ObjectTrackerOperator:
                 _state._last_tracker_run_completion_time - world_time
         return tracker_runtime
 
-    def _reinit_tracker(self, camera_frame, detected_obstacles):
+    @staticmethod
+    def _reinit_tracker(state, camera_frame, detected_obstacles):
         start = time.time()
-        result = self._tracker.reinitialize(camera_frame, detected_obstacles)
+        result = state.tracker.reinitialize(camera_frame, detected_obstacles)
         return (time.time() - start) * 1000, result
 
-    def _run_tracker(self, camera_frame):
+    @staticmethod
+    def _run_tracker(state, camera_frame):
         start = time.time()
-        result = self._tracker.track(camera_frame)
+        result = state.tracker.track(camera_frame)
         return (time.time() - start) * 1000, result
 
 
--- sinks/linear_predictor_sink.py
+++ sinks/linear_predictor_sink.py
@@ -13,10 +13,10 @@ class MySink(Sink):
     def run(self, _ctx, _state, input):
         data = pickle.loads(input.data)
         print("sink input_data : {}".format(data))
-        out_poss_path = os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + os.path.sep + ".")
-        out_poss_path = os.path.join(out_poss_path, "test_data/LinearPredictorOperator/output/linear_prediction_msg-5724.pkl")
-        out_data = pickle.load(open(out_poss_path, "rb"))
-        print("sink out_data : {}".format(out_data))
+        # out_poss_path = os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + os.path.sep + ".")
+        # out_poss_path = os.path.join(out_poss_path, "test_data/LinearPredictorOperator/output/linear_prediction_msg-5724.pkl")
+        # out_data = pickle.load(open(out_poss_path, "rb"))
+        # print("sink out_data : {}".format(out_data))
 def register():
     return MySink
 
