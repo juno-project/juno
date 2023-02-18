d843756b7f3dff2fb1d57eb915c5fbfd6cba7019
charles <leilis@qq.com>
2023-02-18 20:07:57 +0800

    Update utils.py

--- pylot/perception/neat_bev/neat/utils.py
+++ pylot/perception/neat_bev/neat/utils.py
@@ -1,7 +1,19 @@
 import math
 import numpy as np
 import torch
-
+# MIT License
+#
+# Copyright (c) 2018 Tom Runia
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to conditions.
+#
+# Author: Tom Runia
+# Date Created: 2018-08-03
 
 def make_colorwheel():
     """
